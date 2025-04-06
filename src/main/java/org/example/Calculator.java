b2b416ed1a0fcb3a99a416527ebe1d8bb0a354f1
NgoVuong1 <testlab5po@gmail.com>
2025-04-06 19:40:12 +0700

    test fail

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,16 +1,11 @@
 package org.example;
 
 public class Calculator {
-    public static int divide(int a, int b) {
-        if (b == 0) {
-            throw new ArithmeticException("Không thể chia cho 0!");
-        }
-        return a / b;
+    public Integer sum(Integer a, Integer b) {
+        return 0;
     }
-    public static int parseNumber(String input) {
-        if (input == null) {
-            throw new NullPointerException("Chuỗi đầu vào bị null!");
-        }
-        return Integer.parseInt(input);
+
+    public Integer sub(Integer a, Integer b) {
+        return 0;
     }
 }
