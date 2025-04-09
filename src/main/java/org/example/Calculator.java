af10bd9dc952ef2cf1bab8c22c0cefe020a18753
NgoVuong1 <testlab5po@gmail.com>
2025-04-09 18:21:07 +0700

    Sửa code nhánh dev

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Calculator {
     public Integer sum(Integer a, Integer b) {
-        return a + b;
+        return 0;
     }
 
     public Integer sub(Integer a, Integer b) {
